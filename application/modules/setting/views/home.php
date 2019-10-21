7cac1f9dee126780e7eb1166a81bad019cfc5181
Frendi <frendifdp@gmail.com>
2019-10-21 17:52:40 +0700

    setting home & welcome

---
+++ application/modules/setting/views/home.php
@@ -0,0 +1,66 @@
+<table class="table table-bordered" style="cursor: pointer">
+    <tr>
+        <th>Title White</th>
+        <td>
+            <div ondblclick="onedit('title_wihite', 'home')" id="title_wihite">
+                <p style="padding: 0; margin: 0" id="title_wihite1"><?= $home->title_wihite ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Title Red</th>
+        <td>
+            <div ondblclick="onedit('Title_red', 'home')" id="Title_red">
+                <p style="padding: 0; margin: 0" id="Title_red1"><?= $home->Title_red ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Middle Title</th>
+        <td>
+            <div ondblclick="onedit('midle_title', 'home')" id="midle_title">
+                <p style="padding: 0; margin: 0" id="midle_title1"><?= $home->midle_title ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Desc</th>
+        <td>
+            <div ondblclick="onedit('desc', 'home')" id="desc">
+                <p style="padding: 0; margin: 0" id="desc1"><?= $home->desc ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Image 1</th>
+        <td>
+            <div ondblclick="onedit('img_1', 'home')" id="img_1">
+                <p style="padding: 0; margin: 0" id="img_11"><?= $home->img_1 ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Image 2</th>
+        <td>
+            <div ondblclick="onedit('img_2', 'home')" id="img_2">
+                <p style="padding: 0; margin: 0" id="img_21"><?= $home->img_2 ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Image 3</th>
+        <td>
+            <div ondblclick="onedit('img_3', 'home')" id="img_3">
+                <p style="padding: 0; margin: 0" id="img_31"><?= $home->img_3 ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Title White 2</th>
+        <td>
+            <div ondblclick="onedit('title_wihite_2', 'home')" id="title_wihite_2">
+                <p style="padding: 0; margin: 0" id="title_wihite_21"><?= $home->title_wihite_2 ?></p>
+            </div>
+        </td>
+    </tr>
+</table>
---
+++ application/modules/setting/views/welcome.php
@@ -0,0 +1,72 @@
+<table class="table table-bordered" style="cursor: pointer">
+    <tr>
+        <th>Black Title</th>
+        <td>
+            <div ondblclick="onedit('black_title', 'welcome')" id="black_title">
+                <p style="padding: 0; margin: 0" id="black_title1"><?= $welcome->black_title ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Red Title</th>
+        <td>
+            <div ondblclick="onedit('red_title', 'welcome')" id="red_title">
+                <p style="padding: 0; margin: 0" id="red_title1"><?= $welcome->red_title ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Description</th>
+        <td>
+            <div ondblclick="onedit('description', 'welcome')" id="description">
+                <p style="padding: 0; margin: 0" id="description1"><?= $welcome->description ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Desc</th>
+        <td>
+            <div ondblclick="onedit('desc', 'welcome')" id="desc">
+                <p style="padding: 0; margin: 0" id="desc1"><?= $welcome->desc ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Image</th>
+        <td>
+            <div ondblclick="onedit('image', 'welcome')" id="image">
+                <p style="padding: 0; margin: 0" id="image1"><?= $welcome->image ?></p>
+            </div>
+        </td>
+    </tr>
+    <?php for($i = 1; $i<=3; $i++){ 
+        $label = 'label_title_'.$i;
+        $desc = 'label_description_'.$i;
+        $icon = 'icon_'.$i;
+    ?>
+    <tr>
+        <th>Label Title<?=$i?></th>
+        <td>
+            <div ondblclick="onedit('<?=$label?>', 'welcome')" id="<?=$label?>">
+                <p style="padding: 0; margin: 0" id="<?=$label?>1"><?= $welcome->$label ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Label Description<?=$i?></th>
+        <td>
+            <div ondblclick="onedit('<?=$desc?>', 'welcome')" id="<?=$desc?>">
+                <p style="padding: 0; margin: 0" id="<?=$desc?>1"><?= $welcome->$desc ?></p>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <th>Icon <?=$i?></th>
+        <td>
+            <div ondblclick="onedit('<?=$icon?>', 'welcome')" id="<?=$icon?>">
+                <p style="padding: 0; margin: 0" id="<?=$icon?>1"><?= $welcome->$icon ?></p>
+            </div>
+        </td>
+    </tr>
+    <?php } ?>
+</table>
